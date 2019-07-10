3fc2a2a3167ba2bfbdb63e83b4352c42f5a7a2f4
adamc66 <adam.cote66@gmail.com>
2019-07-10 15:05:19 -0400

    exercise 3 is complete

---
+++ ex3.py
@@ -0,0 +1,14 @@
+def is_even(num):
+    if num%2 == 1:
+        return(False)
+    else:
+        return(True)
+
+
+print(is_even(12))
+print(is_even(35))
+print(is_even(89))
+print(is_even(14))
+print(is_even(124))
+print(is_even(2))
+print(is_even(-12))
