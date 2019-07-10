de09e02a4eb7e725cc640f46fc7dad485cdb878a
adamc66 <adam.cote66@gmail.com>
2019-07-10 15:02:47 -0400

    exercise 2 complete

---
+++ ex2.py
@@ -0,0 +1,12 @@
+def negative(num):
+    if num >=0:
+        return(False)
+    else:
+        return(True)
+
+
+print(negative(12))
+print(negative(-12))
+print(negative(0))
+print(negative(7654))
+print(negative(-2212))
